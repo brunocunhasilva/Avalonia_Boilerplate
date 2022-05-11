e3211df4acc19e300d2f314098a1ea4c6072700e
brunocunhasilva <bruno.cunhasilva@outsystems.com>
2022-05-11 17:30:08 +0100

    Demo the memory being grabbed by tge NativeMenu

--- Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
+++ Avalonia.Boilerplate/Avalonia.Boilerplate/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -19,11 +20,47 @@ namespace Avalonia.Boilerplate {
 
         private void MyButton_OnClick(object? sender, RoutedEventArgs e)
         {
-            // var myButton = (Button)sender;
-            // myButton.Content = "My Button was clicked";
+            var topMenu = NativeMenu.GetMenu(this);
 
-            var label = this.FindControl<Label>("MyLabel");
-            label.Content = $"Clicks counter: {++clicksCounter}";
+            BuildNativeTopMenus(topMenu);
+
+            // var label = this.FindControl<Label>("MyLabel");
+            // label.Content = $"Clicks counter: {++clicksCounter}";
+        }
+        
+        private void ForGCCollectButton_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GC.Collect();
+        }
+        
+        
+        public void BuildNativeTopMenus(NativeMenu nativeMenu) {
+            if (nativeMenu != null)
+            {
+                var topMenus = new String[] { "TopMenu1", "TopMenu2" };
+                nativeMenu.Items.Clear(); // memory should be cleared when clearing the old menuItems
+                foreach (var topMenu in topMenus) {
+                    var menuItem = new NativeMenuItem(topMenu);
+                    var lotsOfMemory = GetLotsOfMemory();
+                    var menu = new NativeMenu();
+                    menu.NeedsUpdate += delegate {
+                        Console.WriteLine($"Holding to an array with {lotsOfMemory.Length} of length");
+                    };
+                    menuItem.Menu = menu;
+                    nativeMenu.Add(menuItem);
+                }
+            }
+        }
+
+        private const int ArrayLength = 100 * 1024 * 1024;
+        private Byte[] GetLotsOfMemory()
+        {
+            Byte[] lotsOfMemory = new Byte[ArrayLength];
+            for (int i = 0; i < ArrayLength; i++) {
+                lotsOfMemory[i] = 1;
+            }
+
+            return lotsOfMemory;
         }
     }
 }
